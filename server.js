9293117b5b871178dce39ec1d3f157dab94f5445
Brendan Mok <bmok88@gmail.com>
2017-02-08 10:57:19 -0800

    Webpack and initial client/server setup

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, './client/public')));
+var port = process.env.PORT || 8000;
+app.listen(port, function() {
+  console.log("Gauntlet server listening on port:", port);
+})
