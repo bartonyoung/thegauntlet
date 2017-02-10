057dcc1fcf13a9bf1165af42b92c7558e670e638
Brendan Mok <bmok88@gmail.com>
2017-02-10 10:43:37 -0800

    Redux actions/reducers/store

---
+++ client/redux/reducers.js
@@ -0,0 +1,21 @@
+const getId = (state) => {
+  return state.users.reduce((maxId, user) => {
+    return Math.max(user.id, maxId)
+  }, -1) + 1;
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_USER':
+      return Object.assign({}, state, {
+        users: [{
+          user: action.addUser,
+          id: getId(state)
+        }, ...state.users]
+      })
+    default:
+      return state;
+  }
+}
+
+export default reducer
