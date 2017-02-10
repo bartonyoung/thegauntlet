fabbdc3bd5c1b7c17199e2d9004b0e0700474506
Edmund Louie <ELouie@Edmunds-MacBook-Pro.local>
2017-02-09 18:01:27 -0800

    add api routes to routes.js

---
+++ routes/routes.js
@@ -0,0 +1,11 @@
+const routes = require('express').Router();
+const userControllers = require('../db/controllers/userCtrl.js');
+const s3Controller = require('../db/controllers/s3Ctrl.js');
+const challengeController = require('../db/controllers/challengeCtrl.js');
+
+routes.post('/signup', userControllers.signup);
+routes.post('/userUpload', s3Controller);
+routes.get('/allChallenges', challengeController.getAll);
+routes.post('/challenge', challengeController.addOne);
+routes.get('/:challengeName', challengeController.getOne);
+module.exports = routes;
