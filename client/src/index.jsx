a0ec30b3b39371ae54bb44c6fcfcf7a8e7ca0a1f
Brendan Mok <bmok88@gmail.com>
2017-02-10 10:44:34 -0800

    Refactor index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,50 +1,21 @@
 import React from 'react';
-import {render} from 'react-dom';
-import {Router, Route, hashHistory} from 'react-router';
-import Landing from './components/Landing.jsx';
-import Signup from './components/Signup.jsx';
-import Login from './components/Login.jsx';
-import Dash from './components/Dash.jsx';
+import { render } from 'react-dom';
+import App from './components/App.jsx';
+import configureStore from '../redux/store';
+import { Provider } from 'react-redux';
 
-const dummyData = [
-  {
-    username: "Edmund Louie",
-    ranking: "Bronze 5"
-  },
-  {
-    username: "Barton Young",
-    ranking: "Silver 4"
-  },
-  {
-    username: "Taegyu Leem",
-    ranking: "Wood 8"
-  },
-  {
-    username: "Brendan Mok",
-    ranking: "Plastic 10"
-  }
-];
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+let initialState = {
+  users: [{
+    id: 0,
+    username: 'LeBron James'
+  }]
+};
 
-    this.state = {
-      users: dummyData
-    }
-  }
+let store = configureStore(initialState)
 
-  render() {
-    return(
-      <Router history={hashHistory}>
-        <Route path="/" component={Landing} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/login" component={Login} />
-        <Route path='/dash' component={() => (
-          <Dash userInfo={this.state.users} />
-        )} />
-      </Router>
-    );
-  }
-}
-
-render(<App />, document.getElementById('app'));
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+);
