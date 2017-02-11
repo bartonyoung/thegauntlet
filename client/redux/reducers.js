cbeab9fab0f207bde87e0f5864b82f8c41dc586c
bartonyoung <barton.young1986@gmail.com>
2017-02-10 18:11:42 -0800

    Eslint changes.

--- client/redux/reducers.js
+++ client/redux/reducers.js
@@ -3,14 +3,15 @@ const reducer = (state, action) => {
   switch (action.type) {
   case 'ADD_CHALLENGE':
     return Object.assign({}, state, {
+
       challenges: action.payload 
     });
   case 'ADD_RESPONSE':
     return Object.assign({}, state, {
-      responses: action.payload 
+      responses: action.payload
     });
   default:
-    return state;
+    return state; 
   }
 };
 
