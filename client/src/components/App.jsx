a7d5a774428be8cce50e0e811f537f95429f97bd
Edmund Louie <ELouie@Edmunds-MacBook-Pro.local>
2017-02-10 19:52:30 -0800

    more lint changes

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -6,35 +6,13 @@ import Signup from './Signup.jsx';
 import Login from './Login.jsx';
 import Dash from './Dash.jsx';
 
-<<<<<<< 9f9964d8ca460731036a50391834e8a5904ab968
 const dummyData = ['Barton', 'Edmund', 'Taegyu', 'Brendan'];
-=======
-const dummyData = [
-  {
-    username: 'Edmund Louie',
-    ranking: 'Bronze 5',
-  },
-  {
-    username: 'Barton Young',
-    ranking: 'Silver 4',
-  },
-  {
-    username: 'Taegyu Leem',
-    ranking: 'Wood 8',
-  },
-  {
-    username: 'Brendan Mok',
-    ranking: 'Plastic 10',
-  },
-];
->>>>>>> test
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-<<<<<<< 9f9964d8ca460731036a50391834e8a5904ab968
       users: []
     };
 
@@ -48,10 +26,6 @@ class App extends React.Component {
     this.setState({
       users: newChallenge
     });
-=======
-      users: dummyData,
-    };
->>>>>>> test
   }
 
   render() {
@@ -60,17 +34,9 @@ class App extends React.Component {
         <Route path="/" component={Landing} />
         <Route path="/signup" component={Signup} />
         <Route path="/login" component={Login} />
-<<<<<<< 9f9964d8ca460731036a50391834e8a5904ab968
         <Route path='/dash' component={() => (
           <Dash handleSubmitChallenge={this.handleSubmitChallenge} users={this.state.users}/>
         )} />
-=======
-        <Route
-path="/dash" component={() => (
-          <Dash userInfo={this.state.users} />
-        )}
-        />
->>>>>>> test
       </Router>
     );
   }
