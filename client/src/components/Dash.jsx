ac03a11d02d0c0a5763c461c0b15ab581fbb932b
bartonyoung <barton.young1986@gmail.com>
2017-02-12 19:17:51 -0800

    Add React-Bootstrap Nav with login/signup.

--- client/src/components/Dash.jsx
+++ client/src/components/Dash.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ChallengeTable from './ChallengeTable.jsx';
-import {Jumbotron, Col, Row, Button, Grid} from 'react-bootstrap';
+import {Jumbotron, Col, Row, Button, Grid, Nav, NavItem} from 'react-bootstrap';
 
 const Dash = ({challenges, handleSubmitChallenge, dispatch, handleLogout}) => (
   <div>
     <h1>{window.sessionStorage.getItem('key')}</h1>
-    <a href="#" onClick={handleLogout}>logout</a>
-    <ChallengeTable dispatch={dispatch} />
+      <Nav bsStyle="pills">
+        <NavItem href="#" onClick={handleLogout} className="pull-right">Logout</NavItem>
+      </Nav> 
+    <ChallengeTable id="enterChallenge" handleSubmitChallenge={handleSubmitChallenge} dispatch={dispatch} challenges={challenges}/>
   </div>
 );
 
 export default Dash;
+
+    // <Button href="#" onClick={handleLogout}>logout</Button>
