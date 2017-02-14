71e6d3bf3297fe73737c511e611b5368b60cb3a3
bartonyoung <barton.young1986@gmail.com>
2017-02-14 14:16:03 -0800

    Add Upvote reducer.

--- client/redux/reducers.js
+++ client/redux/reducers.js
@@ -1,20 +1,28 @@
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'ADD_CHALLENGE':
-      return Object.assign({}, state, {
-        challenges: action.payload
-      })
-    case 'ADD_RESPONSE':
-      return Object.assign({}, state, {
-        responses: action.payload
-      })
-    case 'ADD_COMMENT':
-      return Object.assign({}, state, {
-        comments: [action.payload, ...state.comments]
-      })
-    default:
-      return state
+  case 'ADD_CHALLENGE':
+    return Object.assign({}, state, {
+      challenges: action.payload
+    });
+  case 'ADD_RESPONSE':
+    return Object.assign({}, state, {
+      responses: action.payload
+    });
+  case 'ADD_COMMENT':
+    return Object.assign({}, state, {
+      comments: [action.payload, ...state.comments] 
+    });
+  case 'UPVOTE':
+    let newChallenges = state.challenges.slice();
+    // console.log('BEFORE', newChallenges[action.payload].upvotes);
+    newChallenges[action.payload].upvotes = newChallenges[action.payload].upvotes + 1;  
+    // console.log('AFTER', newChallenges[action.payload].upvotes);
+    return Object.assign({}, state, {
+      challenges: newChallenges   
+    });    
+  default: 
+    return state;
   }
 };
 
