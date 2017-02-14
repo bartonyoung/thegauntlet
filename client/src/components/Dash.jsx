88a8c0a5379ced50a98482af9924563d3806ca31
bartonyoung <barton.young1986@gmail.com>
2017-02-13 17:25:14 -0800

    Show username when logged in.

--- client/src/components/Dash.jsx
+++ client/src/components/Dash.jsx
@@ -4,9 +4,9 @@ import {Jumbotron, Col, Row, Button, Grid, Nav, NavItem} from 'react-bootstrap';
 
 const Dash = ({challenges, handleSubmitChallenge, dispatch, handleLogout}) => (
   <div>
-    <h1>{window.sessionStorage.getItem('key')}</h1>
-      <Nav bsStyle="pills">
-        <a href="#"><Button onClick={handleLogout} className="pull-right">Logout</Button></a>
+     <Nav bsStyle="pills"> 
+     <h1>{window.sessionStorage.getItem('key')}</h1>
+        <a href="#"><Button onClick={this.handleLogout} className="pull-right">Logout</Button></a>
       </Nav> 
     <ChallengeTable id="enterChallenge" handleSubmitChallenge={handleSubmitChallenge} dispatch={dispatch} challenges={challenges}/>
   </div>
