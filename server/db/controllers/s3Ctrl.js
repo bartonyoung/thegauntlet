fec7c0a37e78c9c4379605472708666b4037595d
Edmund Louie <ELouie@Edmunds-MacBook-Pro.local>
2017-02-13 19:20:54 -0800

    change s3 controller to return file name

--- server/db/controllers/s3Ctrl.js
+++ server/db/controllers/s3Ctrl.js
@@ -3,13 +3,13 @@ var fs = require('fs');
 var S3FS = require('s3fs');
 var s3fsImpl = new S3FS('thegauntletbucket420', {
   accessKeyId: process.env.accessKeyId, //key here
-  secretAccessKey: process.env.secretKeyId //key here
+  secretAccessKey: process.env.secretAccessKey //key here
 });
 
 module.exports = function(file, res) {
   var stream = fs.createReadStream(file.path);
   return s3fsImpl.writeFile(file.originalFilename, stream).then(function() {
     fs.unlink(file.path, function(err) {});
-    res.redirect('/#/dash');
+    res.json(file.originalFilename);
   });
 };
