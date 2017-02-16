e1df60feea31d0476fccd0de3d0ddff00f06c7cd
bartonyoung <barton.young1986@gmail.com>
2017-02-16 11:55:29 -0800

    Simplify navbar component.

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -8,18 +8,7 @@ class NavBar extends React.Component {
 
   handleNav() {
     if (this.props.auth) {
-      if (window.location.hash !== '#/dash') {
-        return ( 
-          <nav className="nav navbar navbar-fixed">
-            <div className="container-fluid">
-              <button onClick={this.props.handleLogout}>Log out</button>
-              <button onClick={this.props.editProfile}>Edit profile</button>
-              <h5 className="navbar-text navbar-right">You are logged in as <a href="" classNavbar="navbar-link">{window.sessionStorage.getItem('key')}</a></h5>
-            </div>  
-          </nav>
-        );
-      } else {
-        return (
+      return (
           <nav className="nav navbar navbar-fixed">
             <div className="container-fluid">
               <button onClick={this.props.handleLogout}>Log out</button>
@@ -27,8 +16,7 @@ class NavBar extends React.Component {
               <h5 className="navbar-text navbar-right">You are logged in as <a href="" className="navbar-link">{window.sessionStorage.getItem('key')}</a></h5>
             </div>
           </nav>
-        );       
-      }
+      );       
     }
   }
   render() {
