19450405fa4b2c40343777670cfa9d07f50f4750
terry <closer@gmail.com>
2017-02-17 15:10:12 -0800

    Create LeaderBoard component

---
+++ client/src/components/LeaderBoard.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import actions from '../../redux/actions';
+import {connect} from 'react-redux';
+
+class LeaderBoard extends React.component {
+  constructor() {
+    super(props);
+  }
+
+  componentDidMount() {
+    let puter = this;
+
+    
+  }
+
+}
