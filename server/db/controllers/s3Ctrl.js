1d15b59da9a7a295c83e1ebfe5add1c777048117
bartonyoung <barton.young1986@gmail.com>
2017-02-18 15:45:23 -0800

    Handle errors.

--- server/db/controllers/s3Ctrl.js
+++ server/db/controllers/s3Ctrl.js
@@ -2,14 +2,18 @@
 var fs = require('fs');
 var S3FS = require('s3fs');
 var s3fsImpl = new S3FS('thegauntletbucket421', {
-  accessKeyId: process.env.accessKeyID, //key here
-  secretAccessKey: process.env.secretAccessKey //key here
+  accessKeyId: process.env.accessKeyID,
+  secretAccessKey: process.env.secretAccessKey
 });
 
 module.exports = function(file, res) {
   var stream = fs.createReadStream(file.path);
   return s3fsImpl.writeFile(file.originalFilename, stream).then(function() {
-    fs.unlink(file.path, function(err) {});
+    fs.unlink(file.path, function(err) {
+      if (err) {
+        console.error(err);
+      }
+    });
     res.json(file.originalFilename);
   });
 };
