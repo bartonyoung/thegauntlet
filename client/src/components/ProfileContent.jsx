937230ebb91c0556d326a0ab6eb0f73f4b577d30
terry <closer@gmail.com>
2017-02-18 09:47:55 -0800

    Add ranks to user profile

--- client/src/components/ProfileContent.jsx
+++ client/src/components/ProfileContent.jsx
@@ -73,26 +73,30 @@ class ProfileContent extends React.Component {
     };
 
     let mappedChallenges = this.props.challenges.map(challenge => {
-      if (challenge.username === this.props.user[0].username) {
-        return (
-          <div>
-            <h4>{challenge.title}</h4>
-            <p>{challenge.description}</p>
-            {checkFile(challenge.filename.split('.').pop(), challenge)}
-          </div>
-        );
+      if (this.props.user[0]) {
+        if (challenge.username === this.props.user[0].username) {
+          return (
+            <div>
+              <h4>{challenge.title}</h4>
+              <p>{challenge.description}</p>
+              {checkFile(challenge.filename.split('.').pop(), challenge)}
+            </div>
+          );
+        }
       }
     });
 
     let mappedResponses = this.props.responses.map(response => {
-      if (response.username === this.props.user[0].username) {
-        return (
-          <div>
-            <h4>{response.title}</h4>
-            <p>{response.description}</p>
-            {checkFile(response.filename.split('.').pop(), response)}
-          </div>
-        );
+      if (this.props.user[0]) {
+        if (response.username === this.props.user[0].username) {
+          return (
+            <div>
+              <h4>{response.title}</h4>
+              <p>{response.description}</p>
+              {checkFile(response.filename.split('.').pop(), response)}
+            </div>
+          );
+        }
       }
     });
 
@@ -136,7 +140,7 @@ class ProfileContent extends React.Component {
           <div>
             Followers:
             {this.props.followers.map((follower, i) => {
-              return <div key={i}>{i + 1}.   {follower.username}</div>;
+              return <div key={i}>{i + 1}.          {follower.username}</div>;
             })}
           </div>
         );
@@ -145,26 +149,32 @@ class ProfileContent extends React.Component {
       }
 
     };
-
-    return (
-      <div width={screen.width}>
-        <div className='profilePicture container'>
-          <div className='profilePicture text'>This is a placeholder for the profile picture editor</div>
-          Username: {this.props.user[0].username} <br />
-          Firstname: {this.props.user[0].firstname} <br />
-          Lastname: {this.props.user[0].lastname} <br />
-          Email: {this.props.user[0].email} <br />
-          Upvotes:{this.props.user[0].upvotes} <br />
-          Followers: {this.numFollowers()} <br />
-        </div><br/>
-        <div>
-          <button onClick={() => this.changeProfileView('all')}>Challenges/Responses</button>
-          <button onClick={() => this.changeProfileView('followers')}>Followers</button>
-          <button onClick={() => this.changeProfileView('mailbox')}>Mailbox</button>
+    if (this.props.user.length) {
+      return (
+        <div width={screen.width}>
+          <div className='profilePicture container'>
+            <div className='profilePicture text'>This is a placeholder for the profile picture editor</div>
+            Username: {this.props.user[0].username} <br />
+            Firstname: {this.props.user[0].firstname} <br />
+            Lastname: {this.props.user[0].lastname} <br />
+            Email: {this.props.user[0].email} <br />
+            Rank# {this.props.ranks.map((rank, index)=>{
+              return {username: rank.username, rank: index + 1};
+            })[0].rank} (
+              {this.props.user[0].upvotes}) <br />
+            Followers: {this.numFollowers()} <br />
+          </div><br/>
+          <div>
+            <button onClick={() => this.changeProfileView('all')}>Challenges/Responses</button>
+            <button onClick={() => this.changeProfileView('followers')}>Followers</button>
+            <button onClick={() => this.changeProfileView('mailbox')}>Mailbox</button>
+          </div>
+          {myView()}
         </div>
-        {myView()}
-      </div>
-    );
+      );
+    } else {
+      return <div></div>;
+    }
   }
 }
 
