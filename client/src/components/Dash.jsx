6f39f45740cce73b28f34eff564b8fddd1a9730f
Brendan Mok <bmok88@gmail.com>
2017-02-20 11:11:53 -0800

    Fix issues with rendering reversed challenges on dash load

--- client/src/components/Dash.jsx
+++ client/src/components/Dash.jsx
@@ -12,34 +12,6 @@ class Dash extends React.Component {
     super(props);
   }
 
-  // componentDidMount() {
-  //   let outer = this;
-  //   $.get('/api/allChallenges').done(data => {
-  //     data = data.reverse();
-  //     outer.props.dispatch(actions.addChallenge(data));
-  //   });
-
-  //   $.get('/api/getLeaders').then(leaders => {
-  //     outer.props.dispatch(actions.getLeaders(leaders.map(leader => parseInt(leader))));
-  //   });
-
-  //   $.get('/api/profile').done(data => {
-  //     outer.props.dispatch(actions.addUser(data));
-  //   });
-    
-  //   $.get('/api/ranks').then((rankData)=>{
-  //     outer.props.dispatch(actions.getRanks(rankData)); 
-  //   }).then(() => {
-  //     $.get('/api/allchallenges')
-  //       .then(data=>{
-  //         outer.props.dispatch(actions.addChallenge(data));
-  //       });
-  //   }); 
-  //   $.get('/api/favorite').done(data => {
-  //     outer.props.dispatch(actions.setFavorites(data));
-  //   });
-  // }
-
   render() {
     return (
       <div>
@@ -48,7 +20,7 @@ class Dash extends React.Component {
           <div className="row">
             <div className="col col-md-2">
               <SideNav />
-            </div>  
+            </div>
               <ChallengeTable dispatch={this.props.dispatch} />
           </div>
         </div>
