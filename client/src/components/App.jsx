ac40166d6e0d21db8e5bb894a94b5013dc659389
terry <closer@gmail.com>
2017-02-20 16:05:50 -0800

    Add if statement to verify user authentication and refactor ranks request.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -30,27 +30,30 @@ class App extends React.Component {
       data = data.reverse();
       outer.props.dispatch(actions.addChallenge(data));
     });
+    if (window.sessionStorage.getItem('key')) {
+      $.get('/api/getLeaders').then(leaders => {
+        outer.props.dispatch(actions.getLeaders(leaders.map(leader => parseInt(leader))));
+      });
 
-    $.get('/api/getLeaders').then(leaders => {
-      outer.props.dispatch(actions.getLeaders(leaders.map(leader => parseInt(leader))));
-    });
+      $.get('/api/profile').done(data => {
+        outer.props.dispatch(actions.addUser(data));
+      });
+      
+      $.get('/api/favorite').done(data => {
+        outer.props.dispatch(actions.setFavorites(data));
+      });
+    }
 
-    $.get('/api/profile').done(data => {
-      outer.props.dispatch(actions.addUser(data));
-    });
-    
     $.get('/api/ranks').then((rankData)=>{
       outer.props.dispatch(actions.getRanks(rankData)); 
-    }).then(() => {
-      $.get('/api/allchallenges')
-        .then(data=>{
-          data = data.reverse();
-          outer.props.dispatch(actions.addChallenge(data));
-        });
-    }); 
-    $.get('/api/favorite').done(data => {
-      outer.props.dispatch(actions.setFavorites(data));
     });
+    // .then(() => {
+    //   $.get('/api/allchallenges')
+    //     .then(data=>{
+    //       data = data.reverse();
+    //       outer.props.dispatch(actions.addChallenge(data));
+    //     });
+    // }); 
   }
 
   handleLogout() {
