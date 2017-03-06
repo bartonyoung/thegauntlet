106c33884737b3bbeed8701051c0b3df277b6883
Brendan Mok <bmok88@gmail.com>
2017-03-05 17:19:28 -0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 var webpack = require('webpack');
 var path = require('path');
-var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 var BUILD_DIR = path.resolve(__dirname, 'client/public');
 var APP_DIR = path.resolve(__dirname, 'client/src');
@@ -30,21 +29,7 @@ var config = {
         loader: 'style-loader!css-loader'
       }
     ]
-  },
-
-  plugins: [
-    new BrowserSyncPlugin({
-      host: 'localhost',
-      port: '3000',
-      proxy: 'http://localhost:8000/'
-    },
-
-      {
-        reload: true
-      }
-
-   )
-  ]
+  }
 
 };
 
