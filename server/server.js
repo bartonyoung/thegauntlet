f183287f07cb1cefae66a033507f8d8858fb1ff5
bartonyoung <barton.young1986@gmail.com>
2017-03-05 19:29:38 -0800

    attempting to deploy.

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(require('express-session')({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USERNAME, 
     password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_URL
+    database: process.env.DATABASE
     
   })
 })
