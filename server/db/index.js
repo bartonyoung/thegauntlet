b3c322928d26c4a5d1523a039cc91a9b5ad17c41
bartonyoung <barton.young1986@gmail.com>
2017-03-05 18:25:36 -0800

    Testing deployment.

--- server/db/index.js
+++ server/db/index.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 const knex = require('knex')({
   client: 'mysql',
   connection: {
-    host: process.env.DATABASE_HOST || '127.0.0.1',
-    user: process.env.DATABASE_USERNAME || 'root',
-    password: process.env.DATABASE_PASSWORD || '',
-    database: process.env.CLEARDB_DATABASE_URL || 'thegauntlet'
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_URL
   },
   useNullAsDefault: true
 });
